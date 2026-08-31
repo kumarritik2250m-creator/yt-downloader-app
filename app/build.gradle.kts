99c018739aad5bd67572c7fb9c7ed5d4d915b6ea
Ritik <test@example.com>
2026-08-31 14:11:01 +0530

    fix native lib extraction for yt-dlp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,9 @@ android {
     }
 
     packaging {
+        jniLibs {
+            useLegacyPackaging = true
+        }
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
